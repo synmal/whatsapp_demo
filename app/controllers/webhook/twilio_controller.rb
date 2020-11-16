5faecfd2f6172d5201450265bddaf17caac3cf51
Malcolm Jeremiah Rajeli <malcolmjeremiahrajeli@gmail.com>
2020-11-16 15:53:34 +0800

    Setup webhook

--- app/controllers/webhook/twilio_controller.rb
+++ app/controllers/webhook/twilio_controller.rb
@@ -1,5 +1,11 @@
 class Webhook::TwilioController < ApplicationController
   def create
-    
+    Message.create_inbound(twilio_params[:From], twilio_params[:Body], twilio_params.as_json)
+    render json: {message: 'Successfully created message'}, status: :created
+  end
+
+  private
+  def twilio_params
+    params.permit(:SmsMessageSid, :NumMedia, :SmsSid, :SmsStatus, :Body, :To, :NumSegments, :MessageSid, :AccountSid, :From, :ApiVersion)
   end
 end
